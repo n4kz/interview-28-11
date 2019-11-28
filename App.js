d25efd1cdfc2daaa11524c9e457214cfbd424fc4
n4kz <n4kz@n4kz.com>
2019-11-28 22:07:12 +0300

    Refactored main reducer, removed state mutation

--- App.js
+++ App.js
@@ -11,14 +11,20 @@ let initialState = {
 
 let store = createStore((state = initialState, action) => {
   switch (action.type) {
-    case 'UPDATE_PEOPLE':
-      state.people = action.people;
-      return state;
-    case 'TOGGLE_THEME':
-      return {
-        ...state,
-        theme: state.theme === 'light' ? 'dark' : 'light',
-      };
+    case 'UPDATE_PEOPLE': {
+      let { people } = action;
+
+      return { ...state, people };
+    }
+
+    case 'TOGGLE_THEME': {
+      let theme = state.theme === 'light'?
+        'dark':
+        'light';
+
+      return { ...state, theme };
+    }
+
     default:
       return state;
   }
